292c4fae23c7a0409bd3cfc00f6f6084d576fe2e
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-13 20:46:47 +0900

    Register RestTemplate @Bean

---
+++ src/main/java/avengers/nexus/AppConfig.java
@@ -0,0 +1,11 @@
+package avengers.nexus;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate(){return new RestTemplate();}
+}
