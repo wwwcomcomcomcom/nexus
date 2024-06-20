d60cba17cab0a4d86e1d073c079b6ba36afd3970
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-20 22:56:02 +0900

    Fix - correct mapping 'oath'->'oauth'

--- src/main/java/avengers/nexus/github/controller/OauthController.java
+++ src/main/java/avengers/nexus/github/controller/OauthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/oath")
+@RequestMapping("/oauth")
 public class OauthController {
     @Autowired
     OauthService oauthService;
