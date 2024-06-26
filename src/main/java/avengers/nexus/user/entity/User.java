20d451db87b6d302efbbe5bd6e01052a68ceb500
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-26 13:50:06 +0900

    Feat - use builder pattern for User

--- src/main/java/avengers/nexus/user/entity/User.java
+++ src/main/java/avengers/nexus/user/entity/User.java
@@ -2,12 +2,18 @@ package avengers.nexus.user.entity;
 
 import avengers.nexus.utils.StringListConverter;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +25,8 @@ public class User {
     private List<String> followers;
     @Convert(converter = StringListConverter.class)
     private List<String> followings;
+
+    public static UserBuilder builder() {
+        return new UserBuilder().followers(new ArrayList<>()).followings(new ArrayList<>());
+    }
 }
