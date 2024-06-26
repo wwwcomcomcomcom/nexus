cb190b995b94ad85da6d3ab346aea2b6cef377f3
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-27 08:59:50 +0900

    Feat - Separate github & gauth token map

--- src/main/java/avengers/nexus/user/repository/TokenMemoryRepository.java
+++ src/main/java/avengers/nexus/user/repository/TokenMemoryRepository.java
@@ -1,36 +1,29 @@
 package avengers.nexus.user.repository;
 
-import avengers.nexus.user.entity.TokenCollection;
 import avengers.nexus.user.entity.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 @Repository
 public class TokenMemoryRepository {
-    private final HashMap<User, TokenCollection> tokenMap = new HashMap<>();
+    private final HashMap<User, String> githubTokenMap = new HashMap<>();
+    private final HashMap<User, String> gauthTokenMap = new HashMap<>();
 
-    public void saveTokens(User user, String githubToken, String gauthToken) {
-        tokenMap.put(user, new TokenCollection(githubToken, gauthToken));
+    public void saveGithubToken(User user, String githubToken) {
+        githubTokenMap.put(user, githubToken);
     }
 
-    public void saveTokens(User user, TokenCollection token) {
-        tokenMap.put(user, token);
+    public void saveGauthToken(User user, String gauthToken) {
+        gauthTokenMap.put(user, gauthToken);
     }
 
-    public TokenCollection getTokens(User user) {
-        return tokenMap.get(user);
+    public Optional<String> getGithubToken(User user) {
+        return Optional.ofNullable(githubTokenMap.get(user));
     }
 
-    public void deleteTokens(User user) {
-        tokenMap.remove(user);
-    }
-
-    public String getGithubToken(User user) {
-        return tokenMap.get(user).getGithubToken();
-    }
-
-    public String getGauthToken(User user) {
-        return tokenMap.get(user).getGauthToken();
+    public Optional<String> getGauthToken(User user) {
+        return Optional.ofNullable(gauthTokenMap.get(user));
     }
 }
