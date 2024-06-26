ca20797413dc43a5477bc33d044b0dfcecf47974
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-26 13:51:22 +0900

    Feat - add all args constructor

--- src/main/java/avengers/nexus/user/dto/UserSignupDto.java
+++ src/main/java/avengers/nexus/user/dto/UserSignupDto.java
@@ -1,8 +1,10 @@
 package avengers.nexus.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UserSignupDto {
     private String name;
     private String password;
