3490f61c709c94413d7af7d1420687597cafd962
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-26 23:38:04 +0900

    Feat - loginWithGithub

--- src/main/java/avengers/nexus/user/service/UserService.java
+++ src/main/java/avengers/nexus/user/service/UserService.java
@@ -1,18 +1,22 @@
 package avengers.nexus.user.service;
 
+import avengers.nexus.github.domain.GithubUser;
+import avengers.nexus.github.service.OauthService;
 import avengers.nexus.user.dto.UserSignupDto;
 import avengers.nexus.user.entity.User;
 import avengers.nexus.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
+
 @Service
+@AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final OauthService oauthService;
 
     public User getUserById(long id) {
         return userRepository.findById(id).orElseThrow(
@@ -35,5 +39,11 @@ public class UserService {
     public User login(String name, String password) {
         //TODO: separate login logic github vs gauth
         throw new UnsupportedOperationException("Not implemented yet");
+    }
+    public User loginWithGithub(String token) {
+        GithubUser user = oauthService.getGithubUserByToken(token);
+        return userRepository.findByGithubId(user.getId()).orElseThrow(()->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")
+        );
     }
 }
