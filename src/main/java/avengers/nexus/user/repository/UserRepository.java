09adfdda2a774b9a0646cb26691978c4f1c47012
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-26 13:50:26 +0900

    Feat - add UserRepository

---
+++ src/main/java/avengers/nexus/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package avengers.nexus.user.repository;
+
+import avengers.nexus.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findById(long id);
+    Optional<User> findByName(String name);
+}
