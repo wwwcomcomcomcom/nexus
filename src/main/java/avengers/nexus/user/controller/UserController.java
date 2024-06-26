3af48d68ad5c5ff011cca22b7e0eba5de28f8029
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-26 14:59:14 +0900

    Feat - UserService DI, signup & login impl

--- src/main/java/avengers/nexus/user/controller/UserController.java
+++ src/main/java/avengers/nexus/user/controller/UserController.java
@@ -1,15 +1,27 @@
 package avengers.nexus.user.controller;
 
 import avengers.nexus.user.dto.UserSignupDto;
+import avengers.nexus.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/user")
 public class UserController {
+    private final UserService userService;
+    UserController(UserService userService) {
+        this.userService = userService;
+    }
     @PostMapping("/signup")
     public String signup(@RequestBody UserSignupDto user) {
-        //TODO: Implement user signup
+        userService.registerUser(user);
         return "User signed up!";
+    }
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UserSignupDto user, HttpServletRequest request) {
+        userService.login(user.getName(), user.getPassword());
+        return ResponseEntity.ok("User logged in!");
     }
 }
