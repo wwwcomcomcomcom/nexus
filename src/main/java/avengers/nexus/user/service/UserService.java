58794ee8086e36320959978d8eb24b18c447dd92
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-26 13:52:07 +0900

    Feat - add UserService
    1. getUserById
    2. getUserByName
    3. registerUser

---
+++ src/main/java/avengers/nexus/user/service/UserService.java
@@ -0,0 +1,36 @@
+package avengers.nexus.user.service;
+
+import avengers.nexus.user.dto.UserSignupDto;
+import avengers.nexus.user.entity.User;
+import avengers.nexus.user.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserById(long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")
+        );
+    }
+
+    public User getUserByName(String name) {
+        return userRepository.findByName(name).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")
+        );
+    }
+
+    public void registerUser(UserSignupDto userSignupDto) {
+        User user = User.builder()
+                .name(userSignupDto.getName())
+                .password(userSignupDto.getPassword())
+                .build();
+        userRepository.save(user);
+    }
+}
