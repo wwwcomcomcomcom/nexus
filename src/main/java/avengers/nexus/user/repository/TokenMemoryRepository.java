b9302c67d2324ba48b4f968317e6305b110164e1
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-06-27 09:05:49 +0900

    Update - remove saving gauth token

--- src/main/java/avengers/nexus/user/repository/TokenMemoryRepository.java
+++ src/main/java/avengers/nexus/user/repository/TokenMemoryRepository.java
@@ -9,21 +9,12 @@ import java.util.Optional;
 @Repository
 public class TokenMemoryRepository {
     private final HashMap<User, String> githubTokenMap = new HashMap<>();
-    private final HashMap<User, String> gauthTokenMap = new HashMap<>();
 
     public void saveGithubToken(User user, String githubToken) {
         githubTokenMap.put(user, githubToken);
     }
 
-    public void saveGauthToken(User user, String gauthToken) {
-        gauthTokenMap.put(user, gauthToken);
-    }
-
     public Optional<String> getGithubToken(User user) {
         return Optional.ofNullable(githubTokenMap.get(user));
-    }
-
-    public Optional<String> getGauthToken(User user) {
-        return Optional.ofNullable(gauthTokenMap.get(user));
     }
 }
