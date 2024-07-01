8419af5c8e2f95dcf547015eea1a64c8a2790162
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-01 20:51:14 +0900

    Feat - add gAuth service to Bean

--- src/main/java/avengers/nexus/AppConfig.java
+++ src/main/java/avengers/nexus/AppConfig.java
@@ -1,5 +1,7 @@
 package avengers.nexus;
 
+import gauth.GAuth;
+import gauth.impl.GAuthImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,4 +10,6 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
     @Bean
     public RestTemplate restTemplate(){return new RestTemplate();}
+    @Bean
+    public GAuth gAuth(){return new GAuthImpl();}
 }
