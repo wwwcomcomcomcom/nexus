1f2136b399343efd0a98faaa4155f500958171b9
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-10 22:40:52 +0900

    Feat - use final on secretKey

--- src/main/java/avengers/nexus/authentication/JWTUtil.java
+++ src/main/java/avengers/nexus/authentication/JWTUtil.java
@@ -12,7 +12,7 @@ import java.util.Date;
 @Component
 public class JWTUtil {
 
-    private SecretKey secretKey;
+    private final SecretKey secretKey;
 
     public JWTUtil(@Value("${jwt.secret}")String secret) {
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
