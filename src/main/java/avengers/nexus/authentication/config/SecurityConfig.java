608b5e5a3bc6f3f8e0350b6b7adbd2f95c011864
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-11 14:48:32 +0900

    Fix - correct filter endpoint

--- src/main/java/avengers/nexus/authentication/config/SecurityConfig.java
+++ src/main/java/avengers/nexus/authentication/config/SecurityConfig.java
@@ -38,14 +38,14 @@ public class SecurityConfig {
 
     @Bean
     public GauthAuthenticationFilter gauthAuthenticationFilter(AuthenticationManager authenticationManager){
-        GauthAuthenticationFilter filter = new GauthAuthenticationFilter("/login/gauth", authenticationManager, jwtUtil, userService);
-        filter.setFilterProcessesUrl("/login/gauth");
+        GauthAuthenticationFilter filter = new GauthAuthenticationFilter("/user/login/gauth", authenticationManager, jwtUtil, userService);
+        filter.setFilterProcessesUrl("/user/login/gauth");
         return filter;
     }
     @Bean
     public GithubAuthenticationFilter githubAuthenticationFilter(AuthenticationManager authenticationManager){
-        GithubAuthenticationFilter filter = new GithubAuthenticationFilter("/login/github", authenticationManager, jwtUtil, userService);
-        filter.setFilterProcessesUrl("/login/github");
+        GithubAuthenticationFilter filter = new GithubAuthenticationFilter("/user/login/github", authenticationManager, jwtUtil, userService);
+        filter.setFilterProcessesUrl("/user/login/github");
         return filter;
     }
 
