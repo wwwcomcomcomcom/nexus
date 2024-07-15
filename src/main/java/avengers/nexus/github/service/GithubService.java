fa9a42d0f065eb0f56053261d28af0dd95919b87
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-15 15:32:03 +0900

    Feat - env client id

--- src/main/java/avengers/nexus/github/service/GithubService.java
+++ src/main/java/avengers/nexus/github/service/GithubService.java
@@ -22,6 +22,9 @@ public class GithubService implements AuthenticateService {
     private String clientId;
     @Value("${github.client-secret}")
     private String clientSecret;
+    @Value("${github.redirect-uri}")
+    private String redirectUri;
+
     private final RestTemplate restTemplate;
 
     public String getAccessToken(String authorizationCode) {
@@ -33,6 +36,7 @@ public class GithubService implements AuthenticateService {
         Map<String, String> body = new HashMap<>();
         body.put("client_id", clientId);
         body.put("client_secret", clientSecret);
+        body.put("redirect_uri", redirectUri);
         body.put("code", authorizationCode);
 
         HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
