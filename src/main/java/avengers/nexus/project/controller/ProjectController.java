f9dfc2eadb65f5627e1677fe88b60099954a7305
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-07-29 01:09:48 +0900

    implement api endpoint

--- src/main/java/avengers/nexus/project/controller/ProjectController.java
+++ src/main/java/avengers/nexus/project/controller/ProjectController.java
@@ -2,12 +2,29 @@ package avengers.nexus.project.controller;
 
 import avengers.nexus.project.service.ProjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/project")
 @RequiredArgsConstructor
 public class ProjectController {
     private final ProjectService projectService;
+
+    @GetMapping("/")
+    ResponseEntity<?> getAllProjects(){
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body("not implemented");
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<?> getProjectById(@PathVariable String projectId){
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body("not implemented");
+    }
+
+    @PostMapping("/")
+    ResponseEntity<?> registerProject(){
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body("not implemented");
+    }
+
 }
