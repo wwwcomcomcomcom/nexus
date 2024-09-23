063f6231608a45ad50a7e6a1651e5e954c8dc070
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-09-23 23:36:15 +0900

    Add member to Project

--- src/main/java/avengers/nexus/project/controller/ProjectController.java
+++ src/main/java/avengers/nexus/project/controller/ProjectController.java
@@ -3,9 +3,14 @@ package avengers.nexus.project.controller;
 import avengers.nexus.project.dto.CreateProjectDto;
 import avengers.nexus.project.entity.Project;
 import avengers.nexus.project.service.ProjectService;
+import avengers.nexus.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,6 +19,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProjectController {
     private final ProjectService projectService;
+    private User getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+        if(user == null) throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not logged in");
+        return user;
+    }
     @GetMapping("/{id}")
     public Project getProject(@PathVariable String id) {
         return projectService.getProject(id);
@@ -34,5 +45,26 @@ public class ProjectController {
     @DeleteMapping("/{id}")
     public void deleteProject(@PathVariable String id) {
         projectService.deleteProject(id);
+    }
+
+    @PostMapping("/{projectId}/member")
+    public ResponseEntity<?> addMember(@PathVariable String projectId, @RequestBody Long memberId) {
+        User user = getAuthenticatedUser();
+        Project project = projectService.getProject(projectId);
+        if(!project.getOwner().equals(user.getId())) {
+            return ResponseEntity.badRequest().body("Only owner can add member!");
+        }
+        if(project.getMembers().contains(memberId)) {
+            return ResponseEntity.badRequest().body("Member already exists!");
+        }
+        if (user.getId().equals(memberId)) {
+            return ResponseEntity.badRequest().body("Owner cannot be added as member!");
+        }
+        try{
+            project.addMember(memberId);
+            return ResponseEntity.ok("Member added successfully!");
+        }catch (Exception e) {
+            return ResponseEntity.badRequest().body("Member addition failed!");
+        }
     }
 }
