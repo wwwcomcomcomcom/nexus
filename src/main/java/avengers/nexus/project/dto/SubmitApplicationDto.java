0efc5ee881eb07320b4d3ad292af74ac716e052b
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-09-24 20:35:33 +0900

    submit application dto

---
+++ src/main/java/avengers/nexus/project/dto/SubmitApplicationDto.java
@@ -0,0 +1,9 @@
+package avengers.nexus.project.dto;
+
+import avengers.nexus.project.entity.Wanted;
+import lombok.Getter;
+
+@Getter
+public class SubmitApplicationDto {
+    private Wanted wanted;
+}
