90a349d5ea6a515adef490df67294339244a8c00
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-09-24 20:36:12 +0900

    submit application method

--- src/main/java/avengers/nexus/project/entity/Wanted.java
+++ src/main/java/avengers/nexus/project/entity/Wanted.java
@@ -12,10 +12,17 @@ public class Wanted {
     private Integer neededMemberCount;
     private List<String> stack;
     private List<Long> applicants;
+    public void submitApplication(Long applicantId) {
+        this.applicants.add(applicantId);
+        this.neededMemberCount--;
+    }
     public Wanted(String role, Integer neededMemberCount, List<String> stack) {
         this.role = role;
         this.neededMemberCount = neededMemberCount;
         this.stack = stack;
         this.applicants = List.of();
+    }
+    public boolean equals(Wanted wanted) {
+        return this.role.equals(wanted.getRole()) && this.stack.equals(wanted.getStack()) && this.neededMemberCount.equals(wanted.getNeededMemberCount());
     }
 }
