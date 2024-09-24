b2f71fa2f8458a5e1418ef0df11fddda3edd8713
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-09-24 21:12:11 +0900

    feature - create wanted to project

--- src/main/java/avengers/nexus/project/controller/ProjectController.java
+++ src/main/java/avengers/nexus/project/controller/ProjectController.java
@@ -6,6 +6,10 @@ import avengers.nexus.project.entity.Project;
 import avengers.nexus.project.entity.Wanted;
 import avengers.nexus.project.service.ProjectService;
 import avengers.nexus.user.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@Tag(name = "Project", description = "프로젝트 관련 API")
 @RestController
 @RequestMapping("/project")
 @RequiredArgsConstructor
@@ -28,14 +33,19 @@ public class ProjectController {
         return user;
     }
     @GetMapping("/{id}")
+    @Operation(summary = "프로젝트 조회", description = "해당 id의 프로젝트를 조회합니다.")
+    @Parameter(name = "id", description = "프로젝트 ID", required = true)
     public Project getProject(@PathVariable String id) {
         return projectService.getProject(id);
     }
     @GetMapping("/")
+    @Operation(summary = "모든 프로젝트 조회", description = "모든 프로젝트를 조회합니다.")
     public List<Project> getAllProject(){
         return projectService.getAllProject();
     }
     @PostMapping("/")
+    @Operation(summary = "프로젝트 생성", description = "프로젝트를 생성합니다.")
+    @Parameter(name = "project", description = "CreateProjectDto", required = true)
     public ResponseEntity<?> createProject(@RequestBody CreateProjectDto project) {
         try {
             Project result = projectService.createProject(project);
@@ -45,6 +55,8 @@ public class ProjectController {
         }
     }
     @DeleteMapping("/{id}")
+    @Operation(summary = "프로젝트 삭제", description = "프로젝트를 삭제합니다.")
+    @Parameter(name = "id", description = "프로젝트 ID", required = true)
     public void deleteProject(@PathVariable String id) {
         projectService.deleteProject(id);
     }
@@ -70,7 +82,28 @@ public class ProjectController {
         }
     }
 
-    @PostMapping("/{projectId}/wanted")
+    @PostMapping("/{projectId}/wanted/")
+    @Operation(summary = "구인구직 생성", description = "Wanted를 Project에 생성합니다.")
+    @Parameter(name = "projectId", description = "프로젝트 ID", required = true)
+    @Parameter(name = "wanted", description = "Wanted", required = true)
+    public ResponseEntity<?> createWanted(@PathVariable String projectId, @RequestBody Wanted wanted) {
+        User user = getAuthenticatedUser();
+        Project project = projectService.getProject(projectId);
+        if(!project.getOwner().equals(user.getId())) {
+            return ResponseEntity.badRequest().body("Only owner can create wanted!");
+        }
+        try {
+            project.addWanted(new Wanted(wanted.getRole(), wanted.getNeededMemberCount(), wanted.getStack()));
+            return ResponseEntity.ok("Wanted created successfully!");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Wanted creation failed!");
+        }
+    }
+
+    @PostMapping("/{projectId}/apllication")
+    @Operation(summary = "구인구직 지원", description = "프로젝트 구인구직에 지원합니다.")
+    @Parameter(name = "projectId", description = "프로젝트 ID", required = true)
+    @Parameter(name = "applicationDto", description = "SubmitApplicationDto, Wanted를 담고 있음.", required = true)
     public ResponseEntity<?> submitApplication(@PathVariable String projectId, @RequestBody SubmitApplicationDto applicationDto) {
         User user = getAuthenticatedUser();
         Project project = projectService.getProject(projectId);
