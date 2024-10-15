51248e57776406a0039a1dcd4e62c295606c79ff
se0hu1 <leesh6541@gmail.com>
2024-10-16 08:23:11 +0900

    Post Dto

---
+++ src/main/java/avengers/nexus/post/dto/PostDto.java
@@ -0,0 +1,30 @@
+package avengers.nexus.post.dto;
+
+import avengers.nexus.post.entity.Post;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class PostDto {
+    private String id;
+    private String title;
+    private String contents;
+    private String author;
+
+    public static PostDto toDto(Post post){
+        return PostDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .contents(post.getContents())
+                .author(post.getAuthor().getName())
+                .build();
+
+    }
+
+
+
+
+}
